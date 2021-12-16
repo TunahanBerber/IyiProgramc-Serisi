80cd7bd53552e68c0a1000de3662e9160594799c
Tunahan Berber <tunahanberber123@gmail.com>
2021-12-16 16:43:38 +0300

    Add project files.

---
+++ LinqProject/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LinqProject
+{
+    //linq özelliği en güçlü programlama dili c#tır
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Category> categories = new List<Category>();
+            {
+                new Category { CategoryId = 1, CategoryName = "Bilgisayar" };
+                new Category { CategoryId = 2, CategoryName = "Telefon" };
+            }
+
+            List<Product> products = new List<Product>
+            {
+                new Product { ProductId = 1, CategoryId = 1, ProductName = "Hp Pavillion", QuantityPerUnit = "32 GB Ram", UnitPrice = 20000, UnitsInStock = 5 },
+                new Product{ProductId = 2, CategoryId = 1, ProductName = "Asus", QuantityPerUnit = "16 GB Ram", UnitPrice = 15000, UnitsInStock = 5 },
+                new Product { ProductId = 3, CategoryId = 1, ProductName = "Monster ", QuantityPerUnit = "32 GB Ram", UnitPrice = 14000, UnitsInStock = 2 },
+                new Product { ProductId = 4, CategoryId = 2, ProductName = "Xiomi", QuantityPerUnit = "128 GB ", UnitPrice = 2700, UnitsInStock = 2 },
+                new Product { ProductId = 5, CategoryId = 2, ProductName = "Apple", QuantityPerUnit = "8 GB Ram", UnitPrice = 4000, UnitsInStock = 5 },
+            };
+
+            Console.WriteLine("Algoritmik----------");
+
+            foreach (var product in products)
+            {
+                if (product.UnitPrice > 5000 && product.UnitsInStock > 3)
+                {
+                    Console.WriteLine(product.ProductName); 
+                }
+                  
+            }
+            Console.WriteLine("Linq---------------- ");
+
+            var result = products.Where(p => p.UnitPrice > 5000 && p.UnitsInStock > 3); //Where bir koşul anlamına geliyor
+
+            foreach (var product in result)
+            {
+                Console.WriteLine(product.ProductName);
+            }
+
+        }
+
+        static List<Product> GetProducts(List<Product>products)
+        {
+            List<Product> filteredProducts = new List<Product>();
+            foreach (var product in products)
+            {
+                if (product.UnitPrice > 5000 && product.UnitsInStock > 3)
+                {
+                    filteredProducts.Add(product);
+                }
+
+                GetProducts(products);
+
+            }
+
+            return filteredProducts;
+        }
+
+
+        static List<Product> GetProductsLinq(List<Product> products)
+        {
+            return products.Where(product => product.UnitPrice > 5000 && product.UnitsInStock > 3).ToList();
+        }
+
+
+    }
+
+    class Product
+    {
+        public int ProductId { get; set; }
+        public int CategoryId { get; set; } // misal evlenen kadın soyadını değiştirirken soyadını tek kurumda değiştirmesi yeterlidir
+        public string ProductName { get; set; }
+        public string QuantityPerUnit { get; set; }
+        public decimal UnitPrice { get; set; }
+        public int UnitsInStock { get; set; }
+    }
+}
+
+class Category
+{
+    public int CategoryId { get; set; }
+    public string CategoryName { get; set; }
+
+}
+
---
+++ Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace ReferencesTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int,decimal,float,enum,boolean value types
+            //int sayi1 = 10;
+            //int sayi2 = 20;
+
+            //sayi1 = sayi2;
+            //sayi2 = 100;
+
+            //Console.WriteLine("Sayı1 : " + sayi1);
+
+            //int[] sayilar1 = new int[] { 1, 2, 3 };       //int[] ile inti karıştırma array,class interface reference types'tır 
+            //int[] sayilar2 = new int[] { 10, 20, 30 };
+
+            //sayilar1 = sayilar2;
+            //sayilar2[0] = 1000;
+
+            //Console.WriteLine("Sayilar1 [0] = " + sayilar1[0]);
+
+            Person person1 = new Person();
+            Person person2 = new Person();
+
+            person1.FirstName = "Tunahan";
+
+            person2 = person1;
+            person1.FirstName = "Büşra";
+
+            Console.WriteLine(person2.FirstName);
+
+            Customer customer = new Customer();
+            customer.FirstName = "Deniz";
+            customer.CreditCartNumber = "1235456";
+            Employee employee = new Employee();
+            employee.FirstName = "Aybüke";
+
+            Person person3 = customer;              //BASEClass olduğu için kızmaz
+
+            //Console.WriteLine(((Customer)person3).CreditCartNumber);
+
+            PersonManager personManager = new PersonManager();
+            personManager.Add(customer);
+        }
+
+        class Person
+        {
+            public int id { get; set; }
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+        }
+
+        class Customer : Person
+        {
+            public string CreditCartNumber { get; set; }
+        }
+
+        class Employee : Person
+        {
+            public int EmployeeNumber { get; set; }
+        }
+
+        class PersonManager
+        {
+            public void Add(Person person)
+            {
+                Console.WriteLine(person.FirstName);
+            }
+        }
+    }
+}
